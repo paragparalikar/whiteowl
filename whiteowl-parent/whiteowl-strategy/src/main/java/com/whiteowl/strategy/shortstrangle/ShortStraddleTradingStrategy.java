a114e20485cb94a0841ccfdca6820e66a4999912
Parag Paralikar <parag.paralikar@gmail.com>
2022-09-08 21:56:31 +0530

    na

--- whiteowl-parent/whiteowl-strategy/src/main/java/com/whiteowl/strategy/shortstrangle/ShortStraddleTradingStrategy.java
+++ whiteowl-parent/whiteowl-strategy/src/main/java/com/whiteowl/strategy/shortstrangle/ShortStraddleTradingStrategy.java
@@ -25,9 +25,7 @@ import com.whiteowl.strategy.TradingStrategy;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Getter
 @RequiredArgsConstructor
 public class ShortStraddleTradingStrategy implements TradingStrategy {
@@ -41,16 +39,15 @@ public class ShortStraddleTradingStrategy implements TradingStrategy {
 			return handleNewPosition(position);
 		} else if(PositionStatus.OPEN.equals(position.getStatus())) {
 			return handleOpenPosition(position);
-		} else {
-			log.error("Position with status {} should not reach this point : {}", position.getStatus(), position);
-			return false;
 		}
+		return false;
 	}
 	
 	private boolean handleNewPosition(Position position) {
 		final LocalTime now = LocalTime.now();
-		if(now.isAfter(config.getMinPositionOpenTime()) 
-				&& now.isBefore(config.getMaxPositionOpenTime())) {
+		if(position.getEntryTrades().isEmpty() && 
+				now.isAfter(config.getMinPositionOpenTime()) && 
+				now.isBefore(config.getMaxPositionOpenTime())) {
 			open(position);
 			return true;
 		}
@@ -58,23 +55,27 @@ public class ShortStraddleTradingStrategy implements TradingStrategy {
 	}
 	
 	private boolean handleOpenPosition(Position position) {
-		final Set<Scrip> exitScrips = position.getExitTrades().stream()
-				.map(Trade::getScrip).collect(Collectors.toSet());
-		return position.getEntryTrades().stream()
-			.filter(trade -> !exitScrips.contains(trade.getScrip()))
-			.filter(this::shouldClose)
-			.map(Trade::complement)
-			.map(exitTrade -> position.getExitTrades().add(exitTrade))
-			.anyMatch(Predicate.isEqual(Boolean.TRUE));
+		if(2 > position.getExitTrades().size()) {
+			final Set<Scrip> exitScrips = position.getExitTrades().stream()
+					.map(Trade::getScrip).collect(Collectors.toSet());
+			return position.getEntryTrades().stream()
+				.filter(trade -> !exitScrips.contains(trade.getScrip()))
+				.filter(trade -> shouldClose(trade, !exitScrips.isEmpty()))
+				.map(Trade::complement)
+				.map(exitTrade -> position.getExitTrades().add(exitTrade))
+				.anyMatch(Predicate.isEqual(Boolean.TRUE));
+		}
+		return false;
 	}
 	
-	private boolean shouldClose(Trade trade) {
+	private boolean shouldClose(Trade trade, boolean adjustStopLoss) {
 		if(LocalTime.now().isAfter(config.getMaxPositionCloseTime())) return true;
 		final OptionChainItem item = optionChain.findByScrip(trade.getScrip()).orElse(null);
 		if(null == item) return false; 
 		final double entryPrice = trade.getAveragePrice();
 		final double targetPrice = entryPrice * (1 + config.getPercentageTarget() / 100);
-		final double stopLossPrice = entryPrice * (1 - config.getPercentageStopLoss() / 100);
+		final double stopLossPercentage = adjustStopLoss ? 0 : config.getPercentageStopLoss();
+		final double stopLossPrice = entryPrice * (1 - stopLossPercentage / 100);
 		final double currentPrice = item.getLastTradedPrice();
 		return currentPrice >= targetPrice || currentPrice <= stopLossPrice;
 	}
