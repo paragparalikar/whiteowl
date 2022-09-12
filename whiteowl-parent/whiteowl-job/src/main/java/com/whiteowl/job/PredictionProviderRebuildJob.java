478381cc5022d7dfb47af5db09aab6ab176ab1fa
Parag Paralikar <parag.paralikar@gmail.com>
2022-09-12 18:21:58 +0530

    na

---
+++ whiteowl-parent/whiteowl-job/src/main/java/com/whiteowl/job/PredictionProviderRebuildJob.java
@@ -0,0 +1,28 @@
+package com.whiteowl.job;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.whiteowl.core.prediction.PredictionProvider;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class PredictionProviderRebuildJob {
+
+	private final Duration ttl = Duration.ofDays(7);
+	private final List<PredictionProvider> predictionProviders;
+	
+	@Scheduled(cron = "0 0 9 * * *")
+	@EventListener(ApplicationReadyEvent.class)
+	public void run() {
+		predictionProviders.forEach(predictionProvider -> predictionProvider.rebuild(ttl));
+	}
+	
+}
--- whiteowl-parent/whiteowl-job/src/main/java/com/whiteowl/job/RecommendationJob.java
+++ whiteowl-parent/whiteowl-job/src/main/java/com/whiteowl/job/RecommendationJob.java
@@ -8,6 +8,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RecommendationJob {
 
-
+	
 
 }
