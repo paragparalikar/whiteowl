cec447a541067872e4f481fd80866cb4e8ad5ee1
Parag Paralikar <parag.paralikar@gmail.com>
2023-04-09 15:49:09 +0530

    na

--- whiteowl-parent/whiteowl-core/src/main/java/com/whiteowl/core/derivative/option/chain/OptionChainReport.java
+++ whiteowl-parent/whiteowl-core/src/main/java/com/whiteowl/core/derivative/option/chain/OptionChainReport.java
@@ -2,6 +2,7 @@ package com.whiteowl.core.derivative.option.chain;
 
 import static com.whiteowl.core.scrip.ScripType.CE;
 import static com.whiteowl.core.scrip.ScripType.PE;
+import static com.whiteowl.core.scrip.StrikeType.ITM;
 import static com.whiteowl.core.scrip.StrikeType.OTM;
 
 import java.util.Set;
@@ -14,24 +15,30 @@ public class OptionChainReport {
 
 	private double pcrVolume, pcrOi, pcrPrice;
 	private double pcrOtmVolume, pcrOtmOi, pcrOtmPrice;
+	private double callOiRatio, callVolumeRatio, putOiRatio, putVolumeRatio;
 
 	public OptionChainReport(OptionChain optionChain) {
 		final Set<Option> puts = optionChain.getOptions(PE);
 		final Set<Option> calls = optionChain.getOptions(CE);
 		final Set<Option> otmCalls = optionChain.getOptions(CE, OTM);
 		final Set<Option> otmPuts = optionChain.getOptions(PE, OTM);
+		final Set<Option> itmCalls = optionChain.getOptions(CE, ITM);
+		final Set<Option> itmPuts = optionChain.getOptions(PE, ITM);
 		
-		this.pcrOi = computePcr(puts, calls, Option::getOi);
-		this.pcrPrice = computePcr(puts, calls, Option::getLtp);
-		this.pcrVolume = computePcr(puts, calls, Option::getVolume);
-		this.pcrOtmOi = computePcr(otmPuts, otmCalls, Option::getOi);
-		this.pcrOtmPrice = computePcr(otmPuts, otmCalls, Option::getLtp);
-		this.pcrOtmVolume = computePcr(otmPuts, otmCalls, Option::getVolume);
+		this.pcrOi = computeRatio(puts, calls, Option::getOi);
+		this.pcrPrice = computeRatio(puts, calls, Option::getLtp);
+		this.pcrVolume = computeRatio(puts, calls, Option::getVolume);
+		this.pcrOtmOi = computeRatio(otmPuts, otmCalls, Option::getOi);
+		this.pcrOtmPrice = computeRatio(otmPuts, otmCalls, Option::getLtp);
+		this.pcrOtmVolume = computeRatio(otmPuts, otmCalls, Option::getVolume);
+		this.callOiRatio = computeRatio(otmCalls, itmCalls, Option::getOi);
+		this.callVolumeRatio = computeRatio(otmCalls, itmCalls, Option::getVolume);
+		this.putOiRatio = computeRatio(otmPuts, itmPuts, Option::getOi);
+		this.putVolumeRatio = computeRatio(otmPuts, itmPuts, Option::getVolume);
 	}
 	
-	private double computePcr(Set<Option> puts, Set<Option> calls, ToDoubleFunction<? super Option> function) {
-		return puts.stream().mapToDouble(function).sum() / calls.stream().mapToDouble(function).sum();
+	private double computeRatio(Set<Option> numerators, Set<Option> denominators, ToDoubleFunction<? super Option> function) {
+		return numerators.stream().mapToDouble(function).sum() / denominators.stream().mapToDouble(function).sum();
 	}
-	
 	
 }
