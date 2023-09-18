cf5c21386dcca677e2c3361ce21270f6c95fba13
Parag Paralikar <parag.paralikar@gmail.com>
2023-09-18 12:07:21 +0530

    na

--- whiteowl-parent/whiteowl-developer/src/main/java/com/whiteowl/developer/indicator/ATRIndicator.java
+++ whiteowl-parent/whiteowl-developer/src/main/java/com/whiteowl/developer/indicator/ATRIndicator.java
@@ -4,23 +4,10 @@ import java.util.List;
 
 import com.whiteowl.developer.Bar;
 
-public class ATRIndicator implements Indicator {
-
-	private final float[] values;
+public class ATRIndicator extends SMAIndicator {
 	
 	public ATRIndicator(List<Bar> bars, int barCount) {
-		final int size = bars.size();
-		values = new float[size];
-	}
-
-	@Override
-	public float getValue(int index) {
-		return values[index];
-	}
-	
-	@Override
-	public int getSize() {
-		return values.length;
+		super(new TRIndicator(bars), barCount);
 	}
 
 }
--- whiteowl-parent/whiteowl-developer/src/main/java/com/whiteowl/developer/indicator/TRIndicator.java
+++ whiteowl-parent/whiteowl-developer/src/main/java/com/whiteowl/developer/indicator/TRIndicator.java
@@ -4,31 +4,26 @@ import java.util.List;
 
 import com.whiteowl.developer.Bar;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class TRIndicator implements Indicator {
 	
-	private final float[] values;
-
-	public TRIndicator(List<Bar> bars) {
-		final int size = bars.size();
-		values = new float[size];
-		values[size - 1] = Float.NaN;
-		for(int index = size - 2; index >= 0; index--) {
-			final Bar bar = bars.get(index);
-			final Bar previousBar = bars.get(index + 1);
-			values[index] = Math.max(bar.high - bar.close, 
-					Math.max(Math.abs(bar.high - previousBar.close), 
-							Math.abs(previousBar.close - bar.low)));
-		}
-	}
+	private final List<Bar> bars;
 
 	@Override
 	public float getValue(int index) {
-		return values[index];
+		if(index == bars.size() - 1) return Float.NaN;
+		final Bar bar = bars.get(index);
+		final Bar previousBar = bars.get(index + 1);
+		return Math.max(bar.high - bar.close, 
+				Math.max(Math.abs(bar.high - previousBar.close), 
+						Math.abs(previousBar.close - bar.low)));
 	}
 	
 	@Override
 	public int getSize() {
-		return values.length;
+		return bars.size();
 	}
 
 }
